f4903fc0b07dfe7d41bb41ce2a7ee6051733696c
eduardo <linuxmaniaco@gmail.com>
2024-09-21 20:02:06 -0300

    feature / Novo projeto config-server

---
+++ src/test/java/com/example/ProjConfig_serverInfnet/ProjConfigServerInfnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjConfig_serverInfnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjConfigServerInfnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
